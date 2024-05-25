c86e0815435f4cd3a43bc13a8283c86329990fd2
mariamnozadze <mariamnozadze97@yahoo.com>
2024-05-25 12:15:12 +0400

    - adding up to n num

--- big_O.js
+++ big_O.js
@@ -1,1 +1,23 @@
-console.log("wazaap")
+/**
+ * WRITE a function that calculates the sum of all numbers from 1 up to (and including) some number n
+ */
+
+
+//---solution 1
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+      total += i;
+    }
+    return total;
+  }
+  
+  
+  //---solution 2
+  function addUpTo(n) {
+    return (n * (n + 1)) / 2;
+  }
+  
+  
+  
+  
